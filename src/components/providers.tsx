bc87c444c311a038bc04bdadbb49a8983fb9dd30
Hidayath <hidayathulla6161@gmail.com>
2025-09-15 04:11:27 +0530

    fix: Correct filename in provider import

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AuthProvider } from '../hooks/useAuth';
+import { AuthProvider } from '../hooks/use-auth';
 import { Toaster } from './ui/toaster';
 
 export function Providers({ children }: { children: React.ReactNode }) {
